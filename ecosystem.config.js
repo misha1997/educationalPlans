85f38ed749351671459b2c929e2bccdaadc3f2ce
Helsis <seagull.bogdan@gmail.com>
2019-07-09 19:57:18 +0300

    fix ( server overload )

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    apps : [{
-        name        : "worker",
-        script      : "cd server && npm run start",
-        watch      : true,
-    },{
-        name       : "api-app",
-        script     : "cd client && npm run start",
-        watch       : true,
-    }]
+	apps: [{
+		name: "ep server",
+		script: "server/src/index.js",
+		watch: true,
+	}, {
+		name: "ep client",
+		script: "cd client && npm run start",
+		watch: true,
+	}]
 }
