2c18a775d861eb81e89375b3e01f1fe572bfa8ee
Helsis <seagull.bogdan@gmail.com>
2019-07-09 21:27:22 +0300

    fix ( remove running client devServer )

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,5 @@ module.exports = {
 		name: "ep server",
 		script: "server/src/index.js",
 		watch: false,
-	}, {
-		name: "ep client",
-		script: "cd client && npm run start",
-		watch: false,
 	}]
 }
